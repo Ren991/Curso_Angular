5184641538c6c647ef69fd0c4dcc8826cd6d4255
unknown <beccrenzo@gmail.com>
2024-05-19 14:45:05 -0300

    funciones simples en TS

---
+++ Bases_de_TypeScript/src/topics/03-functions.ts
@@ -0,0 +1,23 @@
+
+//Funcion básica en TS
+
+function addNumbers(a:any,b:any){ // Si no se declara lo que devuelve la función por defecto devuelve VOID ( osea nada )
+    return a+b
+}
+
+const addNumbersArrow = (a: number, b:number): string => {
+    return `${a + b}`; // regresar string
+}
+
+function multiply( firstNumber : number, secondNumber?: number, base:number = 2){ // En este caso base tiene por defecto el num 2 en caso que no se envien parámetros. 
+    return firstNumber * base;
+}
+
+const result: number = addNumbers(1,2)
+const result2: string = addNumbersArrow(1,2)
+const multiplyResult: number = multiply(5)
+
+
+console.log({result, result2, multiplyResult})
+
+export {};
