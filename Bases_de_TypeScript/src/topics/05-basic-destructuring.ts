3d07584be4372d522de3d28d07eb45592fdda2b3
Ren991 <beccrenzo@gmail.com>
2024-05-20 16:08:47 -0300

    destructuración básica

---
+++ Bases_de_TypeScript/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,31 @@
+// Desestructuración: Extraer piezas que nos interesan.
+
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration:number;
+    song: string;
+    details: Details
+}
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author:"Pep",
+        year:2015
+    }
+}
+
+const {song:anotherSong , songDuration:duration, details} = audioPlayer; // Desestructurar
+
+const {author} = details;
+console.log("Song:", anotherSong);
+console.log("Duration", duration);
+console.log("Author",author);
+
+export {};
