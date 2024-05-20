b4c6a81dc47e7821378ee1a845070fc3e40781e1
Ren991 <beccrenzo@gmail.com>
2024-05-20 15:49:33 -0300

    práctica funcion con interfaces

--- Bases_de_TypeScript/src/main.ts
+++ Bases_de_TypeScript/src/main.ts
@@ -1,6 +1,7 @@
-import "./topics/01-basic-types" // importamos el modulo de ts
-import "./topics/02-object-interface"
-import "./topics/03-functions"
+//import "./topics/01-basic-types" // importamos el modulo de ts
+//import "./topics/02-object-interface"
+//import "./topics/03-functions"
+import "./topics/04-homework-types";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  HOLA MUNDO
