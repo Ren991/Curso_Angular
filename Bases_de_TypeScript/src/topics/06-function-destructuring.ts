79dc7e5acd4f61efa7fa6007b08566c3971eac22
Ren991 <beccrenzo@gmail.com>
2024-05-22 09:57:08 -0300

    desestructuracion en funciones

---
+++ Bases_de_TypeScript/src/topics/06-function-destructuring.ts
@@ -0,0 +1,51 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+
+const phone: Product = {
+    description: "Nokia A1",
+    price: 150.0
+}
+const tablet: Product = {
+    description: "Ipad Air",
+    price:250.0,
+}
+
+interface TaxCalculationOptions { // un objeto
+
+    tax:  number;
+    products: Product[];
+
+}
+
+//function taxCalculation ( options: TaxCalculationOptions): [number,number] {
+//function taxCalculation ( {tax,products}: TaxCalculationOptions): [number,number] {
+function taxCalculation ( options: TaxCalculationOptions): [number,number] {
+
+    const {tax,products} = options;
+    let total = 0;
+
+    products.forEach(({price}) => {
+        total += price;
+    });
+
+    return [total, total * tax];
+}
+
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [total,taxTotal] = taxCalculation({
+    products: shoppingCart,
+    tax:tax
+});
+
+console.log("total", total);
+console.log("Tax", taxTotal);
+
+
+export {};
