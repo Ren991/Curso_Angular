d7260cb90d1f774a954b81b65f9fe64ccec0772d
Ren991 <beccrenzo@gmail.com>
2024-05-23 10:31:00 -0300

    Priorizar composición sobre herencia

--- Bases_de_TypeScript/src/topics/08-classes.ts
+++ Bases_de_TypeScript/src/topics/08-classes.ts
@@ -3,7 +3,9 @@ export class Person {
     // private address: string; // propiedad privada.
 
     constructor(
-        public name: string,
+        public firstName: string,
+        public lastName: string,
+    
         private address: string = "No address" // Por defecto si no se instancia con address se almacena el valor "No Address"
     ) {
        
@@ -11,7 +13,8 @@ export class Person {
 
 }
 
-export class Hero extends Person{
+/* export class Hero extends Person{
+
      constructor(
         public alterEgo:string,
         public age: number,
@@ -19,7 +22,21 @@ export class Hero extends Person{
      ){
         super( realName, "New York");
     }
-}
+} */
+ export class Hero {
 
-const ironman = new Hero("Ironman",45, "Tony Stark");
+    //public person: Person;
+     constructor(
+        public alterEgo:string,
+        public age: number,
+        public realName: string,
+        public person : Person
+     ){
+       //this.person = new Person(realName);
+    }
+} 
+
+
+const tony = new Person("Tony","Stark", "New York")
+const ironman = new Hero("Ironman",45, "Tony Stark", tony);
 console.log(ironman);
