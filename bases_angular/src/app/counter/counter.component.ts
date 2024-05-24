b7855be67cb442b906ad2baf6b150702977b1c12
Ren991 <beccrenzo@gmail.com>
2024-05-24 15:07:10 -0300

    creación componente counter

---
+++ bases_angular/src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+// .component => es componente. 
+import { Component } from "@angular/core";
+
+@Component({ //=> Decorador
+    selector: 'app-counter',
+    template: '<h1>Hola Counter</h1>',
+})
+export class CounterComponent {
+
+}
