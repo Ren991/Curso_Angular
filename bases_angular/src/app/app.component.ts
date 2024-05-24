f9ddcba900a8d118efb608df68dc037093dd6a77
Ren991 <beccrenzo@gmail.com>
2024-05-24 11:11:22 -0300

    contador en angular

--- bases_angular/src/app/app.component.ts
+++ bases_angular/src/app/app.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
   public counter: number = 10;
+
+  increaseBy(value:number): void {
+    this.counter += value
+  }
+
+  reset(){
+    this.counter = 10;
+  }
+
+  
 }
