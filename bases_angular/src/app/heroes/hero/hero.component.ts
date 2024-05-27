aeedf1679c239a61fba73bb58c7e26c6abde4a21
Ren991 <beccrenzo@gmail.com>
2024-05-27 14:41:19 -0300

    método para cambiar edad y nombre

--- bases_angular/src/app/heroes/hero/hero.component.ts
+++ bases_angular/src/app/heroes/hero/hero.component.ts
@@ -17,4 +17,13 @@ export class HeroComponent {
     
     return `${this.name}- ${this.age}`;
   }
+
+  changeHero():void {
+    this.name ="Spiderman"
+  }
+
+  changeAge():void {
+    this.age = 25
+  }
+
 }
