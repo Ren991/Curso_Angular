4f2be40c7fa6ba9408a58b5680ccab1e34c82344
Ren991 <beccrenzo@gmail.com>
2024-05-27 10:32:25 -0300

    crear componentes con cli y directorios

--- bases_angular/src/app/app.module.ts
+++ bases_angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent // SE importó el contador 
+    CounterComponent,// SE importó el contador 
+    HeroComponent, 
+    ListComponent ,
   ],
   imports: [
     BrowserModule,
--- bases_angular/src/app/counter/counter.component.ts
+++ bases_angular/src/app/counter/counter.component.ts
@@ -3,8 +3,22 @@ import { Component } from "@angular/core";
 
 @Component({ //=> Decorador
     selector: 'app-counter',
-    template: '<h1>Hola Counter</h1>',
+    template: `
+    <h4>Counter: {{counter}}</h4>
+    <button (click)="increaseBy(+1)" >+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+    
+    `,
 })
 export class CounterComponent {
-
+    public title: string ="Hola Mundo";
+    public counter: number = 10 ;
+    
+    increaseBy(value:number):void {
+        this.counter += value;
+    }
+    resetCounter(){
+        this.counter = 10;
+    }
 }
