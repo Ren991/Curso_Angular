eaf5d291927becb7dae729bed15d19d11048b2c0
Ren991 <beccrenzo@gmail.com>
2024-05-28 15:28:13 -0300

    modulos en angular

--- bases_angular/src/app/app.module.ts
+++ bases_angular/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,// SE importó el contador 
     HeroComponent, 
     ListComponent ,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
