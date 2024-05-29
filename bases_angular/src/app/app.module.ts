8235ea68ac9b19c6bbd840fe4feac55e4ad87878
Ren991 <beccrenzo@gmail.com>
2024-05-29 09:36:16 -0300

    fix error importancion

--- bases_angular/src/app/app.module.ts
+++ bases_angular/src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
-import { CounterModule } from './counter/components/counter.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent, 
-    ListComponent ,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    
+    CounterModule,
+    HeroesModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
