99ba4558716a00ce9f80ecf43112245e60eed136
Ren991 <beccrenzo@gmail.com>
2024-06-04 09:53:51 -0300

    paquete externo UUID

--- bases_angular/src/app/dbz/services/dbz.service.ts
+++ bases_angular/src/app/dbz/services/dbz.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core'; // decorador , un servicio es una clase.
+import { v4  as uuid} from 'uuid';
 import { Character } from '../interfaces/character.interface';
+
 
 @Injectable({providedIn: 'root'}) // nuestro servicio va a hacer un singleton en toda la app.
 export class DbzService {
     public characters: Character[] = [{
+        id: uuid(),
         name:"Krillin",
         power:1000
     },{
+        id: uuid(),
         name:"Goku",
         power:9500
     },
     {
+        id: uuid(),
         name:"Vegeta",
         power:7500
     }]
@@ -19,8 +24,11 @@ export class DbzService {
         this.characters.splice(index,1);
       }
 
-    onNewCharacter( character:Character): void{        
-        this.characters.push(character);
+    onNewCharacter( character:Character): void{     
+        
+        const newCharacter: Character = { id: uuid(), ...character };
+
+        this.characters.push(newCharacter);
     }
     
 }
