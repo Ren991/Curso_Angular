d0677fbedc8c9712f53e5f26e77614a140b6a2bb
Ren991 <beccrenzo@gmail.com>
2024-06-12 14:55:07 -0300

    validación de servicio

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -11,8 +11,20 @@ export class GifsService {
         return [...this._tagHistory]
     }
 
-    searchTag( tag: string): void {
+    private organizeHistory(tag: string){
+        tag = tag.toLocaleLowerCase();
+
+        if ( this._tagHistory.includes(tag)){
+            this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+        }
+
         this._tagHistory.unshift(tag);
+        this._tagHistory = this.tagsHistory.splice(0,10);
+    }
+
+    searchTag( tag: string): void {
+        if (tag.length === 0) return ;
+        this.organizeHistory(tag);
     }
     
 }
